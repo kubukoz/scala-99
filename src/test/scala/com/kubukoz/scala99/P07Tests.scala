59c1cbc31b8716eae37988170760a2189009c7a3
Jakub Kozłowski <kubukoz@gmail.com>
2016-07-22 21:47:40 +0200

    Add missing test to P07

--- src/test/scala/com/kubukoz/scala99/P07Tests.scala
+++ src/test/scala/com/kubukoz/scala99/P07Tests.scala
@@ -15,4 +15,8 @@ class P07Tests extends FlatSpec with Matchers {
   it should "flatten a list with a few elements and a few lists" in {
     flatten(List(1, 2, List(3), List(4), Nil, List(List(5), List(List(6, 7))))) shouldBe List(1, 2, 3, 4, 5, 6, 7)
   }
+
+  it should "support the supplied case" in {
+    flatten(List(List(1, 1), 2, List(3, List(5, 8)))) shouldBe List(1, 1, 2, 3, 5, 8)
+  }
 }
