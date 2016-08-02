3712d55d85aa8852b80ad745c2ee18ab090b5332
Jakub Kozłowski <kubukoz@gmail.com>
2016-08-02 18:48:04 +0200

    Add missing task description to P17

--- src/main/scala/com/kubukoz/scala99/P17.scala
+++ src/main/scala/com/kubukoz/scala99/P17.scala
@@ -2,6 +2,14 @@ package com.kubukoz.scala99
 
 import scala.annotation.tailrec
 
+/**
+  * P17 (*) Split a list into two parts.
+  * The length of the first part is given. Use a Tuple for your result.
+  * Example:
+  * *
+  * scala> split(3, List('a, 'b, 'c, 'd, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  * res0: (List[Symbol], List[Symbol]) = (List('a, 'b, 'c),List('d, 'e, 'f, 'g, 'h, 'i, 'j, 'k))
+  **/
 object P17 {
   /**
     * Pattern matching all the way.
